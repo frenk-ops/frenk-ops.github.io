94bc9f61109cf46eb20b3e9884d9817b0a338a95
frenk-ops <308080865+frenk-ops@users.noreply.github.com>
2026-09-22 19:24:36 +0000

    Update Arcane Duels Pages build

--- arcane-duels/app/service-worker.js
+++ arcane-duels/app/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const BUILD_REVISION = "0.23.2-7469255";
+const BUILD_REVISION = "0.23.2-339ce25";
 const SHELL_CACHE = `arcane-duels-shell-${BUILD_REVISION}`;
 const RUNTIME_CACHE = `arcane-duels-runtime-${BUILD_REVISION}`;
 
