31efc52800af934636897f664b1580d23c915626
frenk-ops <kekkopar@gmail.com>
2026-09-23 09:50:19 +0200

    Publish Sigian 0.23.7 cache fff1cda

--- sigian/app/service-worker.js
+++ sigian/app/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const BUILD_REVISION = "0.23.6-sigian-a38efc5";
+const BUILD_REVISION = "0.23.7-sigian-fff1cda";
 const SHELL_CACHE = `sigian-shell-${BUILD_REVISION}`;
 const RUNTIME_CACHE = `sigian-runtime-${BUILD_REVISION}`;
 
