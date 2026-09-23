e6f3458965765bdc17dcd9137eae757f6d795bc7
frenk-ops <kekkopar@gmail.com>
2026-09-23 11:08:52 +0200

    Deploy Sigian 0.23.9 service worker

--- sigian/app/service-worker.js
+++ sigian/app/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const BUILD_REVISION = "0.23.8-sigian-61e5ef6";
+const BUILD_REVISION = "0.23.9-sigian-4bd0bb4";
 const SHELL_CACHE = `sigian-shell-${BUILD_REVISION}`;
 const RUNTIME_CACHE = `sigian-runtime-${BUILD_REVISION}`;
 
