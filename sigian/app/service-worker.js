b6a13065f5450ebb9d5b383120aee7328253d325
frenk-ops <308080865+frenk-ops@users.noreply.github.com>
2026-09-25 16:31:48 +0000

    Update Sigian Pages build

--- sigian/app/service-worker.js
+++ sigian/app/service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const BUILD_REVISION = "0.50.0-f745126";
+const BUILD_REVISION = "0.50.0-487ebb2";
 const SHELL_CACHE = `sigian-shell-${BUILD_REVISION}`;
 const RUNTIME_CACHE = `sigian-runtime-${BUILD_REVISION}`;
 
